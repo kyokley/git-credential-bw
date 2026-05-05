c07e90a15a29d3ae452a606dc0ab58719d98ef77
Kevin Yokley <kevin.yokley@oracle.com>
2026-05-05 09:08:26 -0500

    build(nix-system)(flake): manage flake outputs with flake-parts
    - add flake-parts input
    - define x86_64-linux through flake-parts systems
    - move package definition into perSystem
    - move home-manager module under flake.homeManagerModules.default
    - resolve package helper path from pkgs.stdenv.hostPlatform.system

--- flake.nix
+++ flake.nix
@@ -1,62 +1,67 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = {
+  outputs = inputs @ {
     self,
-    nixpkgs,
+    flake-parts,
     ...
-  }: let
-    pkgs = import nixpkgs {system = "x86_64-linux";};
-    lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
-    version = builtins.substring 0 8 lastModifiedDate;
-  in {
-    packages.x86_64-linux.git-credential-bw = pkgs.stdenv.mkDerivation {
-      pname = "git-credential-bw";
-      inherit version;
-      src = ./.;
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux"];
 
-      nativeBuildInputs = [
-      pkgs.makeWrapper
-      ];
-      buildInputs = [
-        pkgs.bitwarden-cli
-      ];
-      installPhase = ''
-        mkdir -p $out/bin
+      perSystem = {pkgs, ...}: let
+        lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+        version = builtins.substring 0 8 lastModifiedDate;
+      in {
+        packages.git-credential-bw = pkgs.stdenv.mkDerivation {
+          pname = "git-credential-bw";
+          inherit version;
+          src = ./.;
 
-        cp ./git-credential-bw $out/bin/git-credential-bw
-        chmod a+x $out/bin/git-credential-bw
-        wrapProgram $out/bin/git-credential-bw \
-          --set GIT_CREDENTIAL_BW_CMD "${pkgs.bitwarden-cli}/bin/bw"
-      '';
-    };
+          nativeBuildInputs = [
+            pkgs.makeWrapper
+          ];
+          buildInputs = [
+            pkgs.bitwarden-cli
+          ];
+          installPhase = ''
+            mkdir -p $out/bin
 
-    homeManagerModules.default = {
-      config,
-      lib,
-      pkgs,
-      ...
-    }: {
-      options.git-credential-bw = {
-        enable = lib.mkOption {
-          type = lib.types.bool;
-          description = ''
-            Whether to enable the scm-helpers service
+            cp ./git-credential-bw $out/bin/git-credential-bw
+            chmod a+x $out/bin/git-credential-bw
+            wrapProgram $out/bin/git-credential-bw \
+              --set GIT_CREDENTIAL_BW_CMD "${pkgs.bitwarden-cli}/bin/bw"
           '';
         };
       };
 
-      config = lib.mkIf config.git-credential-bw.enable {
-        programs.git = {
-          settings = {
-            credential = {
-              helper = "${self.packages.x86_64-linux.git-credential-bw}/bin/git-credential-bw";
+      flake.homeManagerModules.default = {
+        config,
+        lib,
+        pkgs,
+        ...
+      }: {
+        options.git-credential-bw = {
+          enable = lib.mkOption {
+            type = lib.types.bool;
+            description = ''
+              Whether to enable the scm-helpers service
+            '';
+          };
+        };
+
+        config = lib.mkIf config.git-credential-bw.enable {
+          programs.git = {
+            settings = {
+              credential = {
+                helper = "${self.packages.${pkgs.stdenv.hostPlatform.system}.git-credential-bw}/bin/git-credential-bw";
+              };
             };
           };
         };
       };
     };
-  };
 }
